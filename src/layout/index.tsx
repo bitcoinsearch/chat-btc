1c88571ef5f21035efb6c60ac44a8bce2b0cf8f2
Jonas <jonas@chaincode.com>
2023-07-22 09:31:46 -0400

    another attempt at the twitter image

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -11,11 +11,12 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <meta name="description" content="A conversational AI based on technical bitcoin sources" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <meta property="og:image" content="https://chat.bitcoinsearch.xyz/chat-btc.png"></meta>
+        <meta property="og:image" content="https://chat.bitcoinsearch.xyz/chat-btc-square.png"></meta>
         <meta property="og:title" content="ChatBTC"></meta>
         <meta property="og:url" content="https://chat.bitcoinsearch.xyz"></meta>
         <meta property="og:description" content="A conversational AI based on technical bitcoin sources"></meta>
         <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:image" content="https://chat.bitcoinsearch.xyz/chat-btc-landscape_v1.png"></meta>
         <meta name="twitter:site" content="@chaincodelabs"></meta>
         <meta name="twitter:creator" content="@chaincodelabs"></meta>
       </Head>
