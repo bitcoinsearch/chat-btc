74bec91016a299cc57043fe1cfb8540a5ed99c7b
elraphty <elraphtyofficial@gmail.com>
2023-07-26 14:09:15 +0100

    successfully ran server

--- boltwallServer/src/index.ts
+++ boltwallServer/src/index.ts
@@ -1,5 +1,10 @@
 import app from "./app";
+import dotenv from "dotenv";
 
-app.listen(5000, () => {
-  console.log("Listening");
+dotenv.config();
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, (): void => {
+  console.log(`Server Running here 👉 https://localhost:${PORT}`);
 });
