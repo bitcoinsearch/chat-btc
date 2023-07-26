df0d4bb41d0f119b7e4b118c05872257c58bea7d
elraphty <elraphtyofficial@gmail.com>
2023-07-26 14:22:18 +0100

    added boltwall

--- boltwallServer/src/app.ts
+++ boltwallServer/src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { boltwall } from "boltwall";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(boltwall());
 
 export default app;
