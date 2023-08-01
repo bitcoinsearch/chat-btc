cc1ca9683347814f7e5aee4364080149313a5990
Emmanuel-Develops <itakpeemma@gmail.com>
2023-08-01 16:59:42 +0100

    feat: info dialog and 1 freebie before payment

--- src/pages/api/invoice/index.ts
+++ src/pages/api/invoice/index.ts
@@ -32,7 +32,7 @@ export default async function handler(
       const { payment_request, r_hash } = response.data;
       console.log({ payment_request, r_hash });
       //Todo! save the r_hash in the database
-      fs.appendFileSync("payment.txt", r_hash + "\n", "utf-8");
+      // fs.appendFileSync("payment.txt", r_hash + "\n", "utf-8");
       return res
         .status(200)
         .json({ payment_request, r_hash, amountInSatoshis });
