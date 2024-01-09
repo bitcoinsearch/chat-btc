e26b617cc01a26c915f8d7e7bc103c34cd07b9a2
IgboPharaoh <solomoneze62@gmail.com>
2024-01-09 14:55:21 +0100

    make rating section accept detailed feedback

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -2,7 +2,19 @@ import { useEffect, useRef, useState } from "react";
 import { ThumbDownIcon, ThumbUpIcon } from "@/chakra/custom-chakra-icons";
 import { SupaBaseDatabase } from "@/database/database";
 import { AnswerQuality, FeedbackPayload, Ratings } from "@/types";
-import { Button, Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Text,
+  Textarea,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
 
 import Feedback from "./feedback";
 
@@ -26,6 +38,8 @@ const defaultFeedback = {
 const Rating = ({ isResponseGenerated, feedbackId }: RatingProps) => {
   const [feedback, setFeedback] = useState<FeedbackPayload>(defaultFeedback);
   const [isFeedbackSubmitted, setIsFeedbackSubmitted] = useState(false);
+  const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
+  const [feedbackInput, setFeedbackInput] = useState("");
   const feedbackRef = useRef<HTMLDivElement | null>(null);
 
   const showAnswerQuality =
@@ -66,16 +80,21 @@ const Rating = ({ isResponseGenerated, feedbackId }: RatingProps) => {
     return null;
   }
 
+  const openFeedbackModal = async (feedback: FeedbackPayload) => {
+    return setIsFeedbackOpen(true);
+  };
+
+  const sendDetailedFeedback = async (feedback: FeedbackPayload) => {
+    if (feedbackInput.length === 0) {
+      return;
+    }
+    sendFeedback({ ...feedback, answerQuality: feedbackInput });
+    setIsFeedbackOpen(false);
+  };
+
   return (
-    <Flex
-      flexDir="column"
-      alignItems="flex-start"
-      gap="8"
-      mt="8"
-      pl={4}
-      pb={4}
-    >
-      <Flex flexDir="column" alignItems="flex-start" gap={2}>
+    <Flex flexDir='column' alignItems='flex-start' gap='8' mt='8' pl={4} pb={4}>
+      <Flex flexDir='column' alignItems='flex-start' gap={2}>
         <Text fontWeight={500} fontSize={14}>
           Answer quality
         </Text>
@@ -126,16 +145,58 @@ const Rating = ({ isResponseGenerated, feedbackId }: RatingProps) => {
             What was the issue with this response?
           </Text>
           {Object.entries(AnswerQuality).map(([key, value]) => {
-            return (
-              <Feedback
-                message={value}
-                key={key}
-                feedback={feedback}
-                sendFeedback={sendFeedback}
-              />
-            );
+            return <Feedback message={value} key={key} feedback={feedback} sendFeedback={key === "other" ? openFeedbackModal : sendFeedback} />;
           })}
         </Flex>
+      ) : null}
+
+      {isFeedbackOpen ? (
+        <>
+          {feedback.rating === Ratings.NEGATIVE && Object.keys(AnswerQuality).map((key) => key === "other") ? (
+            <div>
+              <Drawer placement='bottom' onClose={() => setIsFeedbackOpen(false)} isOpen={isFeedbackOpen}>
+                <DrawerOverlay />
+                <DrawerContent>
+                  <DrawerCloseButton />
+                  <DrawerHeader fontSize={{ sm: "16px", md: "20px" }}>Provide additional feedback</DrawerHeader>
+                  <DrawerBody>
+                    <div>
+                      <Textarea
+                        placeholder='please provide more information here'
+                        name=''
+                        id='feedbackInput'
+                        rows={3}
+                        resize='none'
+                        maxH='200px'
+                        onChange={(e) => setFeedbackInput(e.target.value)}
+                        fontSize={{ sm: "14px", md: "16px" }}
+                      />
+                    </div>
+                  </DrawerBody>
+                  <DrawerFooter>
+                    <Button
+                      border={"1px solid white"}
+                      borderRadius={"4px"}
+                      padding={{ sm: "2px 10px", md: "4px 13px" }}
+                      justifyContent={"center"}
+                      fontWeight={"400"}
+                      bg={"transparent"}
+                      outline={"none"}
+                      onClick={() => {
+                        sendFeedback({ ...feedback, answerQuality: feedbackInput });
+                        setIsFeedbackOpen(false);
+                      }}
+                    >
+                      <Text fontSize={"14px"} lineHeight={"18px"}>
+                        Submit feedback
+                      </Text>
+                    </Button>
+                  </DrawerFooter>
+                </DrawerContent>
+              </Drawer>
+            </div>
+          ) : null}
+        </>
       ) : null}
     </Flex>
   );
