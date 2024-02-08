dc665fd8a4db40af22abf234003e87642623ecad
IgboPharaoh <solomoneze62@gmail.com>
2024-02-08 14:14:23 +0100

    fix: modify regex to cater for different patterns

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,19 +1,36 @@
-const splitBodyRegex = /([\s\S]*?)(?=\n\n(?:-\d+-\{\{|\[\d+\]:))/;
-const questionsRegex = /-\d+-\{\{(.*?)\}\}/g;
+const linksRegex = /(^\[\d+\]:\s.*)/gm;
+const questionsRegexIII = /\-[\{]?(\w+|\d+)[\}]?\-[\{]{1,2}(.*[^\}])[\}]{1,2}/g;
 
 export const separateLinksFromApiMessage = (message: string) => {
   const filterFunc = (arg: string, regex: RegExp | string): string[] => {
     return arg
       ?.trim()
       ?.split(regex)
-      .filter((v) => v.length > 1);
+      .filter((v) => v?.length > 1);
   };
 
-  const extractBodyParts = message?.split(splitBodyRegex).filter((v) => v.length > 1);
+  const chunks = message.split(linksRegex).filter((value) => value.length > 1);
+  const bodyAndQuestionsII = filterFunc(chunks[0], questionsRegexIII) ?? [];
 
-  const messageBody = extractBodyParts[0];
-  const messageQuestions = filterFunc(extractBodyParts[1], questionsRegex) ?? [];
-  const messageLinks = filterFunc(extractBodyParts[2], "\n") ?? [];
+  const body = bodyAndQuestionsII[0];
+  const questions =
+    bodyAndQuestionsII
+      .slice(1)
+      .map((i) => {
+        if (i.startsWith("question_")) {
+          i = i.trim().replace("question_", "");
+        } else if (i.startsWith("-")) {
+          i = i.replace("-", "").trim();
+        }
+        return i;
+      })
+      .filter((v) => {
+        return v.trim()?.length > 1;
+      }) ?? [];
+
+  const messageBody = body;
+  const messageQuestions = questions;
+  const messageLinks = chunks.slice(1);
 
   return { messageBody, messageLinks, messageQuestions };
 };
