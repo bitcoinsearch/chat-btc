1f0efbee7c5643dc4e090ebcbce43f92cfb2c780
Emmanuel-Develops <itakpeemma@gmail.com>
2024-03-12 16:09:34 +0100

    fix: enforce answers from context

--- src/config/chatAPI-config.ts
+++ src/config/chatAPI-config.ts
@@ -11,8 +11,9 @@ You are a helpful assistant. You are given a list of user questions, the questio
 export const guidelines = {
   BASE_INSTRUCTION:
     "You are an AI assistant providing helpful answers. You are given the following extracted parts of a long document called CONTEXT BLOCK and a conversation. Provide a conversational detailed answer in the same writing style as based on the context provided. DO NOT include any external references or links in the answers.",
-  NO_ANSWER: `If you are absolutely certain that the answer cannot be found in the context below, just say this phrase '${ERROR_MESSAGES.NO_ANSWER_WITH_LINKS}' EXACTLY. Don't try to make up an answer.`,
+  NO_ANSWER: `If you are absolutely certain that the answer cannot be found in the CONTEXT BLOCK, just say this phrase '${ERROR_MESSAGES.NO_ANSWER_WITH_LINKS}' EXACTLY. DO NOT try to make up an answer that is not in the CONTEXT BLOCK.`,
   UNRELATED_QUESTION: `If the question is not related to the context, say this phrase EXACTLY '${ERROR_MESSAGES.NO_ANSWER}'`,
-  LINKING: `DO NOT explicity mention the existence of the context provided, however, references can to be made to the links provided in the context e.g '[0]'`,
-  FOLLOW_UP_QUESTIONS: `In addition, generate four follow up questions related to the answer generated. Each question should be in this format -{QUESTION_INDEX_HERE}-{{QUESTION_HERE}} and each question should be seperated by a new line. DO NOT ADD AN INTRODUCTORY TEXT TO THE FOLLOW UP QUESTIONS`,
+  LINKING: `DO NOT explicity mention the existence of the context provided, however, references can and should be made to the links provided in the context e.g '[0]'`,
+  FOLLOW_UP_QUESTIONS: `In addition, generate four follow up questions related to the answer generated. Each question should be in this format -{QUESTION_INDEX_HERE}-{{QUESTION_HERE}} and each question should be seperated by a new line. DO NOT ADD AN INTRODUCTORY TEXT TO THE FOLLOW UP QUESTIONS.`,
+  USED_SOURCES: `Lastly, list all sources relevant in generating the answer in a list in this format '__sources__: [LINK_INDICES_HERE]'`
 };
