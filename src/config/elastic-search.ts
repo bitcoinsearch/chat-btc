ce370c19b7396a60d920024b14473a8004105466
Emmanuel-Develops <itakpeemma@gmail.com>
2024-04-06 00:30:54 +0100

    fix: change ES auth config

--- src/config/elastic-search.ts
+++ src/config/elastic-search.ts
@@ -1,13 +1,11 @@
 import { Client } from "@elastic/elasticsearch";
+import { ENV } from "./env";
 
 const client = new Client({
   cloud: {
-    id: process.env.ES_CLOUD_ID as string,
+    id: ENV.ES_CLOUD_ID as string,
   },
-  auth: {
-    username: process.env.ES_USERNAME as string,
-    password: process.env.ES_PASSWORD as string,
-  },
+  auth: { apiKey: process.env.API_KEY as string },
 });
 
 export { client }
