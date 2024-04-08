a374c5cd8df6ef13bc0dd8dd390393be8c4e0395
Emmanuel-Develops <itakpeemma@gmail.com>
2024-04-08 12:27:14 +0100

    fix: change placeholder error message

--- src/pages/api/db/cache.ts
+++ src/pages/api/db/cache.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === "GET") {
     const question = req.query?.question as string | undefined
     const author = req.query?.author as string | undefined
-    if (!question) return res.status(405).json("sd")
+    if (!question) return res.status(405).json({message: "No question asked"})
     const answers = await SupaBaseDatabase.getInstance().getAnswerByQuestion(
       question.toString(),
       author
