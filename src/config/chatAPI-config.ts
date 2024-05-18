5a7b3a713f1f5fbd75f84ae1161fb92fd6747e88
Emmanuel-Develops <itakpeemma@gmail.com>
2024-05-18 03:38:37 +0100

    fix: follow up questions

--- src/config/chatAPI-config.ts
+++ src/config/chatAPI-config.ts
@@ -13,8 +13,8 @@ export const guidelines = {
     "You are an AI assistant providing helpful answers. You are given the following extracted parts of a long document called CONTEXT BLOCK and a conversation. Provide a conversational detailed answer in the same writing style as based on the context provided. DO NOT include any external references or links in the answers.",
   NO_ANSWER: `If you are absolutely certain that the answer cannot be found in the CONTEXT BLOCK, just say this phrase '${ERROR_MESSAGES.NO_ANSWER_WITH_LINKS}' EXACTLY. DO NOT try to make up an answer that is not in the CONTEXT BLOCK.`,
   UNRELATED_QUESTION: `If the question is not related to the context, say this phrase EXACTLY '${ERROR_MESSAGES.NO_ANSWER}'`,
-  LINKING: `DO NOT explicity mention the existence of the context provided, however, references can and should be made to the links provided in the context e.g '[0]'`,
-  FOLLOW_UP_QUESTIONS: `In addition, generate four follow up questions related to the answer generated. Each question should be in this format -{QUESTION_INDEX_HERE}-{{QUESTION_HERE}} and each question should be seperated by a new line. DO NOT ADD AN INTRODUCTORY TEXT TO THE FOLLOW UP QUESTIONS.`,
+  LINKING: `DO NOT explicity mention the existence of the context provided, however, references can and should be made to the links provided in the context e.g '[0]'.`,
+  FOLLOW_UP_QUESTIONS: `If you have an answer, generate four relevant follow up questions, do not include introductory text about follow-up questions. Each question must be in this format: '--{{what problems did segwit solve}}--' in a new line.`,
   USED_SOURCES: `Lastly, list all sources relevant in generating the answer in a list in this format '__sources__: [LINK_INDICES_HERE]'`
 };
 
--- src/utils/rehypeInlineCodeProperty.ts
+++ src/utils/rehypeInlineCodeProperty.ts
@@ -4,9 +4,7 @@ const rehypeInlineCodeProperty = () => {
   return (tree: any) => {
     visit(tree, (node) => {
       if (node.tagName === 'pre') {
-        console.log("pre node", node)
         const codeBlock = node.children[0].tagName === "code" ? node.children[0] : null
-        console.log("codeblock from pre", codeBlock)
         codeBlock.properties.isBlock = true
       }
     });
