ef57151f187e35f782fcef72d3470940b387a5bc
Emmanuel-Develops <itakpeemma@gmail.com>
2024-07-30 16:11:26 +0100

    refactor: consistent naming 'test' for tests

--- src/__tests__/externalDependency/extractor.test.ts
+++ src/__tests__/externalDependency/extractor.test.ts
@@ -2,18 +2,18 @@ import { GPTKeywordExtractor } from "@/service/chat/extractor";
 import { chatHistory, chatHistoryContextAware, chatHistorySwitchContext } from "@/__tests__/__mocks__/mockChatHistory";
 
 describe("GPTKeywordExtractor", () => {
-  it("should extract keywords from a given history", async () => {
+  test("should extract keywords from a given history", async () => {
     const keywords = await GPTKeywordExtractor(chatHistory);
     expect(keywords).toContain("OP_CODES");
   });
 
-  it("should return context aware keywords", async () => {
+  test("should return context aware keywords", async () => {
     const keywords = await GPTKeywordExtractor(chatHistoryContextAware);
     const lowerCaseKeywords = keywords?.map(keyword => keyword.toLowerCase());
     expect(lowerCaseKeywords).toContain("segwit");
   });
 
-  it("should switch context", async () => {
+  test("should switch context", async () => {
     const keywords = await GPTKeywordExtractor(chatHistorySwitchContext);
     expect(keywords).not.toContain("segwit");
   });
