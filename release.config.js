04a38f9cd958ad6f5f75e5dc997f2667d32a5021
Emmanuel-Develops <itakpeemma@gmail.com>
2024-11-06 14:24:56 +0100

    feat: add semantic-release automation

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    '@semantic-release/changelog',
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
