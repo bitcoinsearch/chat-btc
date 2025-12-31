edcc879978a01dd4d45d8a0c4764403606ed2333
0tuedon <90271995+0tuedon@users.noreply.github.com>
2025-12-31 03:33:45 +0100

    chore: boss banner removal 2025

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,8 @@
 import { Box, Divider, Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
-import BossBanner from "../banner/BossBanner";
-import { useRouter } from "next/router";
 
 const Navbar = () => {
-  const router = useRouter();
-  const isHomeScreen = Object.keys(router.query).length < 1;
   return (
     <Box
       as="nav"
@@ -19,7 +15,6 @@ const Navbar = () => {
       isolation="isolate"
       zIndex={1}
     >
-      {isHomeScreen && <BossBanner />}
       <Flex alignItems="center" h="full" px={4} bgColor="gray.900" zIndex={1}>
         <Link href="/">
           <Text>
